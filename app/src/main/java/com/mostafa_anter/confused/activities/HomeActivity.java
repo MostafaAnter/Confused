6b773f812ac2d1437f8af8250bded7486d60edb0
mostafa_anter <ga30000@gmail.com>
2016-09-01 17:30:31 +0200

    stable version

--- app/src/main/java/com/mostafa_anter/confused/activities/HomeActivity.java
+++ app/src/main/java/com/mostafa_anter/confused/activities/HomeActivity.java
@@ -1,4 +1,4 @@
-package com.mostafa_anter.confused;
+package com.mostafa_anter.confused.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.mostafa_anter.confused.R;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
